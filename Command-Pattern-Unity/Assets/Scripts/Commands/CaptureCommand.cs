224f10b342ac38795570f8b4ccfb864ef113c15b
BasarEkinci <basarekinci02@outlook.com>
2023-10-06 12:45:27 +0300

    Capture Command Added

---
+++ Command-Pattern-Unity/Assets/Scripts/Commands/CaptureCommand.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Commands
+{
+    public class CaptureCommand : CommandManager.ICommand
+    {
+        private Vector3Int m_Start;
+        private Vector3Int m_End;
+
+        private Unit m_Captured;
+        private Unit m_Capturing;
+        
+        public CaptureCommand(Vector3Int start, Vector3Int end)
+        {
+            m_Start = start;
+            m_End = end;
+        }
+        
+        public void Execute()
+        {
+            m_Captured = Gameboard.Instance.GetUnit(m_End);
+            m_Capturing = Gameboard.Instance.GetUnit(m_Start);
+            
+            Gameboard.Instance.MoveUnit(m_Capturing,m_End);
+            Gameboard.Instance.TakeOutUnit(m_Captured);
+            Gameboard.Instance.SwitchTeam();
+        }
+
+        public void Undo()
+        {
+            Gameboard.Instance.MoveUnit(m_Capturing,m_Start);
+            Gameboard.Instance.MoveUnit(m_Captured,m_End);
+            Gameboard.Instance.SwitchTeam();
+        }
+    }
+}
